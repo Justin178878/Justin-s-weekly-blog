209a12eb48e5c42dda180720ec2c96e9939be73e
Justin Park <justin.park@mds.ac.nz>
2026-06-14 22:53:56 +1200

    added sixth week blog

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ async function loadData() {
     document.querySelector("#output").innerHTML = `
         <p>Name: ${data.name}</p>
         <p>Age: ${data.age}</p>
-        <p>4th Blog: ${data["4th Blog"]}</p>
+        <p>5th Blog: ${data["5th Blog"]}</p>
         <p>Course: ${data.Course}</p>
     `;
     
